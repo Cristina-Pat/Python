33f8ed14e71cdbf16512a1dbd086be1280e190de
Cristina Pat <calota.cristina@gmail.com>
2021-11-15 22:00:08 +0000

    Created a function for bubble sort

--- algorithms/bubble_sort.py
+++ algorithms/bubble_sort.py
@@ -1,17 +1,21 @@
-test_array = [7, 5, 9, 2, 5]
-a = 5
-b = 3
-c = a # c = 5
-a = b # a = 3
-b = c # b = 5 
+test_array=[]
+num_elements = 0
 
-swapped = True
-while swapped == True:
-    swapped = False
-    for i in range (0, len(test_array) - 1):
-        if test_array[i] > test_array[i + 1]:
-            x = test_array[i + 1]
-            test_array[i + 1] = test_array[i]
-            test_array[i] = x 
-            swapped = True
+while num_elements < 5:
+    numbers = input("Please insert your number: ")
+    test_array.append(numbers)
+    num_elements += 1
+
+def bubble_sort (test_array: list) -> list:
+    swapped = True
+    while swapped == True:
+        swapped = False
+        for i in range (0, len(test_array) - 1):
+            if test_array[i] > test_array[i + 1]:
+                x = test_array[i + 1]
+                test_array[i + 1] = test_array[i]
+                test_array[i] = x 
+                swapped = True
     print(test_array)
+
+bubble_sort(test_array)

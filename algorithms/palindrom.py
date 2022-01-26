c11acf8254245f2dc02fdb7b60e9e640a202c882
Cristina Pat <calota.cristina@gmail.com>
2022-01-26 21:02:26 +0000

    changed from class to fuction isPalindrome

--- algorithms/palindrom.py
+++ algorithms/palindrom.py
@@ -1,19 +1,11 @@
-
-from re import X
-
-
-class Palindrome: 
-    def __init__ (self, aWord):
-        self.word = aWord 
-
-    def isPalindrome(x):
-        if len(x) == 0 or len(x) == 1:
-            result = True
-        if x[0] == x[len-1]:
-            result = isPalindrome(x[1, len-1])
-        result = False
-        print(result)
+def isPalindrome(x):
+    if len(x) == 0 or len(x) == 1:
+        return True
+    elif x[0] == x[len(x)-1]:
+        return isPalindrome(x[1:len(x)-1])
+    return False
 
 
 
-
+print(isPalindrome('beca'))
+print(isPalindrome('racecar'))

8e8aab3267a82e2b9b01a560b9452c838a0636ce
Cristina Pat <calota.cristina@gmail.com>
2022-01-29 18:21:46 +0000

    added insertionSort function for sorting cards

--- algorithms/cards.py
+++ algorithms/cards.py
@@ -13,7 +13,7 @@ VALUES = 'A23456789TJQK'
 def value(card: str) -> int: #return the value of the card
     for i in range(len(VALUES)):
         if VALUES[i] == card[0]:
-            return i + 1 #teh output is 1 to 13 for values, not 0-12(i)
+            return i + 1 #the output is 1 to 13 for values, not 0-12(i)
 
 def suitValue(card: str) -> tuple:
     return(suit(card), value(card))
@@ -23,12 +23,24 @@ def sort(cards: list, key: Callable) -> tuple:
         for j in range(i + 1, len(cards)):
             if key(cards[i]) > key(cards[j]):
                cards[i], cards[j] = cards[j], cards[i]
-
     print (cards)
+    
+def insertionSort(cards: list, key: Callable) -> tuple:
+    for unsorted in range(1, len(cards)):
+        toSort = cards[unsorted]
+        theKey = key(toSort) 
+
+        i = unsorted # to start, index where to put card is index where it is now
+        while i > 0 and key(cards[i - 1]) > theKey: # for each sorted item larger the one to sort
+            cards[i] = cards[i - 1] # shift up to the right
+            i-=1 # proceed with the next sorted item on the left
+        cards[i] = toSort #set the unsorted item
+    print(cards)
 
 #print(suitValue('AS'))
 
 cards = ['2S', 'AS', '2D', 'AD']
-sort(cards, key=suitValue)
+insertionSort(cards, suitValue)
+#sort(cards, key=suitValue)
 #cards.sort(key=suitValue)
 #print(cards)

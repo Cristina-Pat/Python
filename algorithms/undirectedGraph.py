46ba4e1ba5c563bcb423b0a31f1fee3675ea9837
Cristina Pat <calota.cristina@gmail.com>
2022-02-02 13:53:34 +0000

    added an instance for undirectedGraph with methods

--- algorithms/undirectedGraph.py
+++ algorithms/undirectedGraph.py
@@ -1,3 +1,4 @@
+from __future__ import unicode_literals
 from typing import Hashable
 from graphs import Digraph
 
@@ -13,6 +14,7 @@ class UndirectedGraph(Digraph):
         super().removeEdge(node1, node2)
         super().removeEdge(node2, node1)
 
+    #return the graph's edges as a set of pair
     def edges(self) -> set:
         allEdges = set()
         for node1 in self.neighbours:
@@ -20,3 +22,35 @@ class UndirectedGraph(Digraph):
                 if(node2, node1) not in allEdges:
                     allEdges.add((node1, node2))
         return allEdges
+
+    #return all nodes that are adjacent to the node
+    def getNeighbours(self, node: Hashable) -> set:
+        return self.outNeighbours(node)
+
+    #return the number of edges attached to the node
+    def inDegree(self, node: Hashable) -> int:
+        return self.outDegree(node)
+
+    #return the number of edges attached to the node
+    def degree(self, node: Hashable) -> int:
+        return self.outDegree(node)
+
+
+test2 = UndirectedGraph()
+test2.addNode('Tom')
+test2.addNode('Isa')
+test2.addNode('Greg')
+test2.addNode('Alex')
+
+test2.addEdge('Tom', 'Isa')
+test2.addEdge('Tom', 'Alex')
+
+test2.addEdge('Isa', 'Tom')
+test2.addEdge('Isa', 'Alex')
+
+test2.addEdge('Alex', 'Isa')
+test2.addEdge('Alex', 'Tom')
+
+print(test2.edges())
+print(test2.getNeighbours('Alex'))
+print(test2.degree('Tom'))

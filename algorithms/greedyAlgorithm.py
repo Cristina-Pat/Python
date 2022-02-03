3ca8e96f287716ae9bd95ba3a789394e6843b14d
Cristina Pat <calota.cristina@gmail.com>
2022-02-03 19:27:24 +0000

    added implementation for a greedy algorithm

---
+++ algorithms/greedyAlgorithm.py
@@ -0,0 +1,18 @@
+
+coins = [1, 2, 5, 10, 20, 50, 100] #1, 2 ... 50 is 1p, 2p ... 50p and 100 means £1.
+highestCoins = enumerate(reversed(coins)) # allow go backwards through coins list
+
+def giveChange(change, coins):
+    # create a list in size of length coins filled with 0
+    toGive = [0] * len(coins)
+    
+    #loop through the list, but keep the position in another variable, i
+    for i, coin in highestCoins: #the loop starts with coin = 100 and i = 6
+        while coin <= change:
+            change -= coin
+            toGive[i] += 1 #keep track of the counter
+    return(toGive)
+
+
+print(giveChange(60, coins))
+
